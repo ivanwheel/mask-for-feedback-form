9bb89a97fb635fbccc827adebeca9768df4106c3
Ivan Wheel <38592394+ivanwheel@users.noreply.github.com>
2018-04-21 22:42:34 +0300

    Add files via upload

---
+++ mask-for-feedback-form/javascript.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function() {
+		$("#phone").mask("+7 (999) 999 99 99");
+
+	});
+
+		function valid (form){
+		var fail = false;
+		var name = form.name.value;
+		var phone = form.phone.value;
+		var email = form.email.value;
+		var adr_pattern = /[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,5}/i;
+		if (name == "" || name == " ")
+			fail = "You did not enter your name!";
+		else if (phone == "")
+			fail = "You did not enter your phone number!";
+		else if (adr_pattern.test(email) == false)
+			fail = "You have incorrectly entered your mail!";
+
+		if(fail) 
+			alert(fail);
+	}
